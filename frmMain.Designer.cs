d8d81ce4a6ab896dbd79115544fb46a45ee26db2
lzp729 <lzp729@gmail.com>
2017-10-30 20:17:00 +0800

    Initial commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace FileDeploy
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlPanel = new System.Windows.Forms.Panel();
+            this.lblDirection = new System.Windows.Forms.Label();
+            this.clbTargets = new System.Windows.Forms.CheckedListBox();
+            this.clbSources = new System.Windows.Forms.CheckedListBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.chkRemove = new System.Windows.Forms.CheckBox();
+            this.chkDryRun = new System.Windows.Forms.CheckBox();
+            this.pnlPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlPanel
+            // 
+            this.pnlPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlPanel.Controls.Add(this.lblDirection);
+            this.pnlPanel.Controls.Add(this.clbTargets);
+            this.pnlPanel.Controls.Add(this.clbSources);
+            this.pnlPanel.Location = new System.Drawing.Point(12, 42);
+            this.pnlPanel.Name = "pnlPanel";
+            this.pnlPanel.Size = new System.Drawing.Size(760, 270);
+            this.pnlPanel.TabIndex = 0;
+            // 
+            // lblDirection
+            // 
+            this.lblDirection.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblDirection.Location = new System.Drawing.Point(370, 0);
+            this.lblDirection.Name = "lblDirection";
+            this.lblDirection.Size = new System.Drawing.Size(20, 270);
+            this.lblDirection.TabIndex = 2;
+            this.lblDirection.Text = "=>";
+            this.lblDirection.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // clbTargets
+            // 
+            this.clbTargets.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.clbTargets.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.clbTargets.CheckOnClick = true;
+            this.clbTargets.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.clbTargets.Dock = System.Windows.Forms.DockStyle.Right;
+            this.clbTargets.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clbTargets.ForeColor = System.Drawing.Color.Red;
+            this.clbTargets.FormattingEnabled = true;
+            this.clbTargets.Location = new System.Drawing.Point(390, 0);
+            this.clbTargets.Name = "clbTargets";
+            this.clbTargets.Size = new System.Drawing.Size(370, 270);
+            this.clbTargets.TabIndex = 1;
+            this.clbTargets.DoubleClick += new System.EventHandler(this.clbTargets_DoubleClick);
+            // 
+            // clbSources
+            // 
+            this.clbSources.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.clbSources.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.clbSources.CheckOnClick = true;
+            this.clbSources.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.clbSources.Dock = System.Windows.Forms.DockStyle.Left;
+            this.clbSources.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.clbSources.ForeColor = System.Drawing.Color.Green;
+            this.clbSources.FormattingEnabled = true;
+            this.clbSources.Location = new System.Drawing.Point(0, 0);
+            this.clbSources.Name = "clbSources";
+            this.clbSources.Size = new System.Drawing.Size(370, 270);
+            this.clbSources.TabIndex = 0;
+            this.clbSources.DoubleClick += new System.EventHandler(this.clbSources_DoubleClick);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGo.Location = new System.Drawing.Point(12, 351);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(760, 23);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(12, 380);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOutput.Size = new System.Drawing.Size(760, 169);
+            this.txtOutput.TabIndex = 3;
+            this.txtOutput.TextChanged += new System.EventHandler(this.txtOutput_TextChanged);
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(12, 9);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(520, 26);
+            this.lblDescription.TabIndex = 4;
+            this.lblDescription.Text = "Replace each files in right panel folders of the same name with left panel files," +
+    "  recursively into all sub-folders. \r\nDouble-Click or Drag to add files or folde" +
+    "rs.";
+            // 
+            // chkRemove
+            // 
+            this.chkRemove.AutoSize = true;
+            this.chkRemove.Location = new System.Drawing.Point(12, 319);
+            this.chkRemove.Name = "chkRemove";
+            this.chkRemove.Size = new System.Drawing.Size(148, 17);
+            this.chkRemove.TabIndex = 5;
+            this.chkRemove.Text = "Remove all those maches";
+            this.chkRemove.UseVisualStyleBackColor = true;
+            // 
+            // chkDryRun
+            // 
+            this.chkDryRun.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.chkDryRun.AutoSize = true;
+            this.chkDryRun.Checked = true;
+            this.chkDryRun.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkDryRun.Location = new System.Drawing.Point(579, 12);
+            this.chkDryRun.Name = "chkDryRun";
+            this.chkDryRun.Size = new System.Drawing.Size(193, 17);
+            this.chkDryRun.TabIndex = 6;
+            this.chkDryRun.Text = "Dry run ( Count only, no file actions)";
+            this.chkDryRun.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.chkDryRun);
+            this.Controls.Add(this.chkRemove);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.pnlPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "File Deploy";
+            this.pnlPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlPanel;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.CheckedListBox clbSources;
+        private System.Windows.Forms.CheckedListBox clbTargets;
+        private System.Windows.Forms.Label lblDirection;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.CheckBox chkRemove;
+        private System.Windows.Forms.CheckBox chkDryRun;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace FileDeploy
+{
+    public partial class frmMain : Form
+    {
+        List<string> sourceFiles = new List<string>();
+        List<string> targetFolders = new List<string>();
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        
+        private void clbSources_DoubleClick(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "Select files to be deployed";
+                openFileDialog.InitialDirectory = ".";
+                openFileDialog.Filter = "Files to deploy|*";
+                openFileDialog.FilterIndex = 0;
+                openFileDialog.RestoreDirectory = true;
+                openFileDialog.Multiselect = true;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (string file in openFileDialog.FileNames)
+                    {
+                        sourceFiles.Add(file);
+                        this.clbSources.Items.Add(string.Format("{0,-12}", Path.GetFileName(file)) + " (" + Path.GetDirectoryName(Path.GetFullPath(file)) + " )");
+                        this.clbSources.SetItemChecked(this.clbSources.Items.Count - 1, true);
+                    }
+                }
+            }
+        }
+        
+        private void clbTargets_DoubleClick(object sender, EventArgs e)
+        {
+            FolderSelectDialog folderBrowserDialog = new FolderSelectDialog();
+            folderBrowserDialog.Title = "Choose folder to deploy";
+
+            if (folderBrowserDialog.ShowDialog(this.Handle))
+            {
+                string target = folderBrowserDialog.FileName;
+                targetFolders.Add(target);
+                this.clbTargets.Items.Add(target);
+                this.clbTargets.SetItemChecked(this.clbTargets.Items.Count - 1, true);
+            }
+        }
+
+        private void txtOutput_TextChanged(object sender, EventArgs e)
+        {
+            this.txtOutput.SelectionStart = this.txtOutput.TextLength;
+            this.txtOutput.ScrollToCaret();
+        }
+
+        int countPass = 0;
+        int countFail = 0;
+        bool todryrun = false;
+        bool toremove = false;
+        
+        Thread trdReplace = null;
+        volatile bool going = false;
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            if (trdReplace == null)
+            {
+                this.txtOutput.Clear();
+                countPass = 0;
+                countFail = 0;
+                todryrun = this.chkDryRun.Checked;
+                toremove = this.chkRemove.Checked;
+
+                List<string> sources = new List<string>();
+                List<string> targets = new List<string>();
+
+                for (var i = 0; i < this.clbSources.Items.Count; ++i)
+                {
+                    if (this.clbSources.GetItemChecked(i))
+                        sources.Add(sourceFiles[i]);
+                }
+
+                for (var i = 0; i < this.clbTargets.Items.Count; ++i)
+                {
+                    if (this.clbTargets.GetItemChecked(i))
+                        targets.Add(targetFolders[i]);
+                }
+
+                this.txtOutput.Text = sources.Count + " Files => " + targets.Count + " Folders" + Environment.NewLine;
+
+                if (sources.Count > 0 && targets.Count > 0 &&
+                    MessageBox.Show("Good to go?", "File Deploy", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    this.btnGo.Text = "Stop";
+                    going = true;
+                    foreach (string folder in targets)
+                    {
+                        DeployFiles(sources, folder);
+                        if (!going)
+                            break;
+                    }
+                    trdReplace = new Thread(new ThreadStart(
+                        () =>
+                        {
+                            foreach (string folder in targets)
+                            {
+                                DeployFiles(sources, folder);
+                                if (!going)
+                                    break;
+                            }
+
+                            this.Invoke((MethodInvoker)delegate
+                            {
+                                this.btnGo.Enabled = true;
+                                if (todryrun)
+                                    this.txtOutput.Text += Environment.NewLine + Environment.NewLine + "=== Dry run, no files changed ===";
+
+                                if (toremove)
+                                        this.txtOutput.Text += Environment.NewLine + Environment.NewLine + countPass + " files removed";
+                                    else
+                                    this.txtOutput.Text += Environment.NewLine + Environment.NewLine + countPass + " files replaced";
+
+                                this.txtOutput.Text += Environment.NewLine + countFail + " errors";
+                            });
+
+                            EngGo();
+                        }
+                        ));
+                    trdReplace.Start();
+                }
+            }
+            else
+            {
+                going = false;
+                new Thread(new ThreadStart(
+                    () =>
+                    {
+                        trdReplace.Join();
+                        EngGo();
+                    }
+                )).Start();
+            }
+        }
+
+        private void DeployFiles(List<string> files, string folder)
+        {
+            if (!going)
+                return;
+
+            if (!Directory.Exists(folder))
+            //if (!(new DirectoryInfo(folder)).Exists)
+            {
+                if (this.InvokeRequired)
+                {
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        this.txtOutput.Text += Environment.NewLine + "Folder Not Exists - " + folder;
+                        ++this.countFail;
+                    });
+                }
+                return;
+            }
+
+            if (this.InvokeRequired)
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    this.txtOutput.Text += Environment.NewLine + " - " + folder;
+                });
+            }
+
+            if (!going)
+                return;
+
+            try
+            {
+                Directory.GetFiles(folder);
+            }
+            catch
+            {
+                if (this.InvokeRequired)
+                {
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        this.txtOutput.Text += Environment.NewLine + "Failed to Read Folder - " + folder;
+                        ++this.countFail;
+                    });
+                }
+                return;
+            }
+
+            foreach (string file in files)
+            {
+                if (!going)
+                    break;
+
+                string thisDestFile = Path.GetFullPath(folder) + "\\" + Path.GetFileName(file);
+                if (File.Exists(thisDestFile)
+                //if ( (new FileInfo(thisDestFile).Exists)
+                    && !thisDestFile.Equals(Path.GetFullPath(file), StringComparison.OrdinalIgnoreCase)
+                    )
+                {
+                    try
+                    {
+                        if (toremove)
+                        {
+                            if (!todryrun)
+                                File.Delete(thisDestFile);
+
+                            if (this.InvokeRequired)
+                            {
+                                this.Invoke((MethodInvoker)delegate
+                                {
+                                    this.txtOutput.Text += Environment.NewLine + "Removed - " + thisDestFile;
+                                    ++this.countPass;
+                                });
+                            }
+                        }
+                        else
+                        {
+                            if (!todryrun)
+                                File.Copy(file, thisDestFile, true);
+
+                            if (this.InvokeRequired)
+                            {
+                                this.Invoke((MethodInvoker)delegate
+                                {
+                                    this.txtOutput.Text += Environment.NewLine + "OK - " + Path.GetFileName(file) + " => " + folder;
+                                    ++this.countPass;
+                                });
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        if (this.InvokeRequired)
+                        {
+                            this.Invoke((MethodInvoker)delegate
+                            {
+                                this.txtOutput.Text += Environment.NewLine + "Failed to Copy File - " + Path.GetFileName(file) + " => " + folder;
+                                ++this.countFail;
+                            });
+                        }
+                    }
+                }
+            }
+
+            if (!going)
+                return;
+
+            foreach (string desentFolder in Directory.GetDirectories(folder))
+            {
+                if (!going)
+                    break;
+                DeployFiles(files, desentFolder);
+            }
+        }
+        
+        private void EngGo()
+        {
+            trdReplace = null;
+            if (this.InvokeRequired)
+                this.Invoke((MethodInvoker)delegate
+                {
+                    this.btnGo.Text = "Go";
+                });
+        }
+    }
+}
